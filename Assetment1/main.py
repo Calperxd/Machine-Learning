71e4922e830bdf9a2ce488a8e6c9d3497dee6d9c
Gabriel Seabra <gabriel.seabra@itriad.org.br>
2021-11-07 20:20:41 -0400

    update

--- Assetment1/main.py
+++ Assetment1/main.py
@@ -41,44 +41,96 @@ Title_Dictionary = {
 }
 
 def get_titles():
-    # we extract the title from each name
     x['Title'] = x['Name'].map(lambda name: name.split(',')[1].split('.')[0].strip())
-
-    # a map of more aggregated title
-    # we map each title
     x['Title'] = x.Title.map(Title_Dictionary)
     return x
 
 
 def process_embarked():
     global x
-    # two missing embarked values - filling them with the most frequent one in the train  set(S)
     x.Embarked.fillna('S', inplace=True)
-    # dummy encoding
     embarked_dummies = pd.get_dummies(x['Embarked'], prefix='Embarked')
     x = pd.concat([x, embarked_dummies], axis=1)
     x.drop('Embarked', axis=1, inplace=True)
     return x
 
 
-def process_cabin():
+def process_sex():
     global x
-    # replacing missing cabins with U (for Uknown)
-    x.Cabin.fillna('U', inplace=True)
-
-
-    # dummy encoding ...
-    cabin_dummies = pd.get_dummies(x['Cabin'], prefix='Cabin')
-    x = pd.concat([x, cabin_dummies], axis=1)
-
-    x.drop('Cabin', axis=1, inplace=True)
+    x['Sex'] = x['Sex'].map({'male':1, 'female':0})
     return x
+
+
+def process_pclass():
+    
+    global x
+    pclass_dummies = pd.get_dummies(x['Pclass'], prefix="Pclass")
+    x = pd.concat([x, pclass_dummies],axis=1)
+    x.drop('Pclass',axis=1,inplace=True)
+    return x
+
+
+def process_ticket():
+    global x
+    class_mapping = {label: idx for idx, label in enumerate(np.unique(x['Ticket']))}
+    x['Ticket'] = x['Ticket'].map(class_mapping)
+    return x
+
+
+def process_family():
+    global x
+    x['FamilySize'] = x['Parch'] + x['SibSp'] + 1
+    x['Singleton'] = x['FamilySize'].map(lambda s: 1 if s == 1 else 0)
+    x['SmallFamily'] = x['FamilySize'].map(lambda s: 1 if 2 <= s <= 4 else 0)
+    x['LargeFamily'] = x['FamilySize'].map(lambda s: 1 if 5 <= s else 0)
+    return x
+
+
+def compute_score(clf, X, y, scoring='accuracy'):
+    xval = cross_val_score(clf, X, y, cv = 5, scoring=scoring)
+    return np.mean(xval)
+
+
+def recover_train_test_target():
+    global x
+    
+    targets = pd.read_csv('./data/train.csv', usecols=['Survived'])['Survived'].values
+    train = x.iloc[:891]
+    test = x.iloc[891:]
+    
+    return train, test, targets
+
 
 
 
 
 x = get_titles()
 x = process_embarked()
-x = process_cabin()
+x = process_sex()
+x = process_ticket()
+
+train, test, targets = recover_train_test_target()
+clf = RandomForestClassifier(n_estimators=50, max_features='sqrt')
+clf = clf.fit(train, targets)
+features = pd.DataFrame()
+features['feature'] = train.columns
+features['importance'] = clf.feature_importances_
+features.sort_values(by=['importance'], ascending=True, inplace=True)
+features.set_index('feature', inplace=True)
+
+features.plot(kind='barh', figsize=(25, 25))
+model = SelectFromModel(clf, prefit=True)
+train_reduced = model.transform(train)
+print(train_reduced.shape)
+# (891L, 14L)
+
+test_reduced = model.transform(test)
+print(test_reduced.shape)
+# (418L, 14L)
+
+
+
+
+print(x.head(15))
 
 #############################################################################################################
