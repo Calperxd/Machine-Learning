c11d87936fb5f32a380645033034e0e707eb9972
Gabriel Calper Seabra <gabrielseabra@alunos.utfpr.edu.br>
2021-11-07 21:37:51 -0400

    normalizing the datas

--- Assetment1/main.py
+++ Assetment1/main.py
@@ -1,23 +1,15 @@
+#loading the data
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectFromModel
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import StandardScaler
 import numpy as np
 import pandas as pd
 
-
-
-#loading the data
-train_data = pd.read_csv("train.csv", sep=";")
-
 #############################################################################################################
-#pre processing
-train_data = train_data.dropna(subset=['Age'])                                  #removendo linhas da idade que são NaN
-train_data['Cabin'] = train_data['Cabin'].fillna(0)                             #Zerando os que não possuem cabine
-train_data = train_data.dropna(subset=['Embarked'])                             #Removendo os que não estão cadastrados como embarcados
-
-
-x = train_data.iloc[:, 2:]                                          #Features
-y = train_data.iloc[:, 1]                                           #Classe vivo ou morto
-
+train_data = pd.read_csv("train.csv", sep=";")
 titles = set()
-for name in x['Name']:
+for name in train_data['Name']:
     titles.add(name.split(',')[1].split('.')[0].strip())
 
 Title_Dictionary = {
@@ -41,96 +33,92 @@ Title_Dictionary = {
 }
 
 def get_titles():
-    x['Title'] = x['Name'].map(lambda name: name.split(',')[1].split('.')[0].strip())
-    x['Title'] = x.Title.map(Title_Dictionary)
-    return x
+    train_data['Title'] = train_data['Name'].map(lambda name: name.split(',')[1].split('.')[0].strip())
+    train_data['Title'] = train_data.Title.map(Title_Dictionary)
+    return train_data
 
 
 def process_embarked():
-    global x
-    x.Embarked.fillna('S', inplace=True)
-    embarked_dummies = pd.get_dummies(x['Embarked'], prefix='Embarked')
-    x = pd.concat([x, embarked_dummies], axis=1)
-    x.drop('Embarked', axis=1, inplace=True)
-    return x
+    global train_data
+    train_data.Embarked.fillna('S', inplace=True)
+    embarked_dummies = pd.get_dummies(train_data['Embarked'], prefix='Embarked')
+    train_data = pd.concat([train_data, embarked_dummies], axis=1)
+    train_data.drop('Embarked', axis=1, inplace=True)
+    return train_data
 
 
 def process_sex():
-    global x
-    x['Sex'] = x['Sex'].map({'male':1, 'female':0})
-    return x
+    global train_data
+    train_data['Sex'] = train_data['Sex'].map({'male':1, 'female':0})
+    return train_data
 
 
 def process_pclass():
-    
-    global x
-    pclass_dummies = pd.get_dummies(x['Pclass'], prefix="Pclass")
-    x = pd.concat([x, pclass_dummies],axis=1)
-    x.drop('Pclass',axis=1,inplace=True)
-    return x
+    global train_data
+    pclass_dummies = pd.get_dummies(train_data['Pclass'], prefix="Pclass")
+    train_data = pd.concat([train_data, pclass_dummies],axis=1)
+    train_data.drop('Pclass',axis=1,inplace=True)
+    return train_data
 
 
 def process_ticket():
-    global x
-    class_mapping = {label: idx for idx, label in enumerate(np.unique(x['Ticket']))}
-    x['Ticket'] = x['Ticket'].map(class_mapping)
-    return x
+    global train_data
+    class_mapping = {label: idx for idx, label in enumerate(np.unique(train_data['Ticket']))}
+    train_data['Ticket'] = train_data['Ticket'].map(class_mapping)
+    return train_data
 
 
 def process_family():
-    global x
-    x['FamilySize'] = x['Parch'] + x['SibSp'] + 1
-    x['Singleton'] = x['FamilySize'].map(lambda s: 1 if s == 1 else 0)
-    x['SmallFamily'] = x['FamilySize'].map(lambda s: 1 if 2 <= s <= 4 else 0)
-    x['LargeFamily'] = x['FamilySize'].map(lambda s: 1 if 5 <= s else 0)
-    return x
+    global train_data
+    train_data['FamilySize'] = train_data['Parch'] + train_data['SibSp'] + 1
+    train_data['Singleton'] = train_data['FamilySize'].map(lambda s: 1 if s == 1 else 0)
+    train_data['SmallFamily'] = train_data['FamilySize'].map(lambda s: 1 if 2 <= s <= 4 else 0)
+    train_data['LargeFamily'] = train_data['FamilySize'].map(lambda s: 1 if 5 <= s else 0)
+    return train_data
 
 
-def compute_score(clf, X, y, scoring='accuracy'):
-    xval = cross_val_score(clf, X, y, cv = 5, scoring=scoring)
-    return np.mean(xval)
+def process_fare():
+    global train_data
+    stdsc = StandardScaler()
+    X_train_std = stdsc.fit_transform(train_data["Fare"].values)
+    print(X_train_std)
+    #train_data_norm = mms.fit_transform(train_data["Fare"])
+    #print(train_data_norm)
+    #train_data["Fare"] = train_data_norm
+    return train_data
 
+train_data = get_titles()
+train_data = process_pclass()
+train_data = process_embarked()
+train_data = process_sex()
+train_data = process_ticket()
+train_data = process_fare()
 
-def recover_train_test_target():
-    global x
-    
-    targets = pd.read_csv('./data/train.csv', usecols=['Survived'])['Survived'].values
-    train = x.iloc[:891]
-    test = x.iloc[891:]
-    
-    return train, test, targets
-
-
-
-
-
-x = get_titles()
-x = process_embarked()
-x = process_sex()
-x = process_ticket()
-
-train, test, targets = recover_train_test_target()
-clf = RandomForestClassifier(n_estimators=50, max_features='sqrt')
-clf = clf.fit(train, targets)
-features = pd.DataFrame()
-features['feature'] = train.columns
-features['importance'] = clf.feature_importances_
-features.sort_values(by=['importance'], ascending=True, inplace=True)
-features.set_index('feature', inplace=True)
-
-features.plot(kind='barh', figsize=(25, 25))
-model = SelectFromModel(clf, prefit=True)
-train_reduced = model.transform(train)
-print(train_reduced.shape)
-# (891L, 14L)
-
-test_reduced = model.transform(test)
-print(test_reduced.shape)
-# (418L, 14L)
+#pre processing
+train_data = train_data.dropna(subset=['Age'])                                  #removendo linhas da idade que são NaN
+train_data['Cabin'] = train_data['Cabin'].fillna(0)                             #Zerando os que não possuem cabine
 
 
 
+x = train_data.iloc[:, 3:]                                                 #Features
+y = train_data[["Survived"]]                                           #Classe vivo ou morto
+print(x)
 
-print(x.head(15))
+#clf = RandomForestClassifier(n_estimators=50, max_features='sqrt')
+#clf = clf.fit(x, y)
+#features = pd.DataFrame()                                           #Definindo features
+#features['feature'] = x.columns
+#features['importance'] = clf.feature_importances_
+#features.sort_values(by=['importance'], ascending=True, inplace=True)
+#features.set_index('feature', inplace=True)
+#features.plot(kind='barh', figsize=(25, 25))
+
+
+#train = combined.iloc[:891]
+#test = combined.iloc[891:]
+
+#print(x)
+#print(y)
+
 
 #############################################################################################################
